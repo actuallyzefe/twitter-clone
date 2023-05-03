5edf9b287041cab4bdfcb4bae1ae9e3ff660d161
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-05-03 12:09:21 +0300

    removed: boiler plates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
-import { UsersController } from './users/users.controller';
+
 import { TweetModule } from './tweet/tweet.module';
 
 @Module({
@@ -20,7 +18,5 @@ import { TweetModule } from './tweet/tweet.module';
     AuthModule,
     TweetModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
