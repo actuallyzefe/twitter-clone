5803e1a1a754943676081c97f21d2b3059bb21b7
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-05-11 02:30:50 +0300

    fix: Password excluded fro response

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,5 +1,4 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
-import { Exclude, Expose } from 'class-transformer';
 
 export enum Roles {
   user = 'user',
@@ -10,43 +9,33 @@ export enum Roles {
   timestamps: true,
 })
 export class User {
-  @Expose()
   @Prop()
   name: string;
 
-  @Expose()
   @Prop({ unique: [true, 'This nickname is taken '] })
   nickname: string;
 
-  @Expose()
   @Prop({ unique: [true, 'This email is taken'] })
   email: string;
 
-  @Expose()
   @Prop({ default: 'user' })
   role: Roles;
 
-  @Exclude()
-  @Prop()
+  @Prop({ select: false })
   password: string;
 
-  @Exclude()
   @Prop()
   passwordConfirm: string;
 
-  @Expose()
   @Prop({ default: 'default.jpg' })
   avatar: string;
 
-  @Expose()
   @Prop({ default: '', max: 150 })
   bio: string;
 
-  @Expose()
   @Prop({ ref: 'User', default: [] })
   followers: [];
 
-  @Expose()
   @Prop({ ref: 'User', default: [] })
   followings: [];
 }
